277ce4e56470d13b1982f1e11b4dbf88dbb3c8e7
protarie125 <protarie@gmail.com>
2018-03-27 23:26:49 +0900

    Comment in header file

--- SurMath.h
+++ SurMath.h
@@ -13,12 +13,13 @@ public:
     SurMath();
 };
 
-class Matrix {
+class Matrix { // 매트릭스 클래스를 만들긔
 public:
     // construct
-    Matrix();
-    Matrix(int n);
-    Matrix(int row, int column);
+    Matrix(); // 기본 생성자 = 1차 정방행렬, zero-matrix
+    Matrix(int n); // n차 정방행렬의 생성자
+    Matrix(int row, int column); // row, column을 지정해서 생성
+    //Matrix(type, row, column); // 원하는 type(class)의 data를 가지도록 생성하고 싶음
     // distruct
     ~Matrix();
     // get
@@ -32,7 +33,8 @@ private:
     int m_row;
     int m_column;
     bool m_isSquare;
-    QVector< QVector< int > > m_data;
+    QVector< QVector< int > > m_data; // pointer를 사용하는 편이 좋을지도? 궁금
+    //QVector< QVector< type > > m_type_data // 흠?
 };
 
 #endif // SURMATH_H
